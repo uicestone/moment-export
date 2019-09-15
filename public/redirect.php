d1b8a4e5fd4b8713091a8e2b0e0c58b3794fd167
Uice Lu <uicestone@gmail.com>
2019-09-16 00:32:43 +0800

    fix short url json file path

--- public/redirect.php
+++ public/redirect.php
@@ -1,6 +1,6 @@
 <?php
 
-$json = file_get_contents('./data/shorten_urls.json');
+$json = file_get_contents('../data/shorten_urls.json');
 $shorten_urls = json_decode($json);
 $hash = $_SERVER['QUERY_STRING'];
 $url = $shorten_urls->$hash;
