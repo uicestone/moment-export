44275de750d2cad0f44d131f4c85ad3931a3a3b1
Uice Lu <uicestone@gmail.com>
2019-09-15 22:52:30 +0800

    write shorten url json

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import MomentItem from "./interfaces/MomentItem";
 import fixRawData from "./utils/fixRawData";
 import { renderFile } from "pug";
 import QRCode from "qrcode";
-import md5 from "md5";
+import { createHash } from "crypto";
 
 const rawJson = readFileSync("./data/exported_sns.json");
 
@@ -11,6 +11,7 @@ const raw = JSON.parse(rawJson.toString()) as MomentItem[];
 
 (async () => {
   const moments = (await fixRawData(raw)).reverse();
+  const shortenUrls: { [shorten: string]: string } = {};
   moments.forEach(moment => {
     const firstLine = moment.content.split("\n")[0];
     if (firstLine.length < 15) {
@@ -18,12 +19,21 @@ const raw = JSON.parse(rawJson.toString()) as MomentItem[];
       moment.content = moment.content.replace(/^.*?\n/, "");
     }
     if (moment.originalPost) {
+      const hash = createHash("md5")
+        .update(moment.originalPost.url)
+        .digest("base64")
+        .replace(/[\/\+]/, "")
+        .substr(0, 4);
       QRCode.toFile(
-        `./public/${md5(moment.originalPost.url)}.png`,
-        moment.originalPost.url
+        `./public/qrcodes/${hash}.png`,
+        // moment.originalPost.url
+        "https://uice.lu/r?" + hash,
+        { margin: 0, errorCorrectionLevel: "M" }
       );
+      shortenUrls[hash] = moment.originalPost.url;
     }
   });
+  writeFileSync("./data/shorten_urls.json", JSON.stringify(shortenUrls));
   // const html = renderFile("./template/index.pug", { moments });
   // writeFileSync("./public/index.html", html);
   const mdHtml = renderFile("./template/md.pug", { moments });
